ccce77f39f52c8cee8827fadf265c838e0fbd016
Andrija Petrovic <andrija.hers@gmail.com>
2016-07-13 14:03:06 +0000

    Init

---
+++ index.js
@@ -0,0 +1,72 @@
+function createModuleRecognition (getAlias) {
+  'use strict';
+  var _allexprefix = 'allex_',
+      _allexsuffices = ['service','parser','storage','lib'],
+      shortNotationExpander = require('./shortnotationresolver')(getAlias);
+
+
+  function checkForSuffix(repo, mmn,resultobj,suffix){
+    var servicename, namespace, secondunderscorepos, group;
+    if(mmn.lastIndexOf(suffix)===mmn.length-suffix.length){
+      servicename = mmn.substring(repo.alias.length+1,mmn.length-suffix.length); //repo+1: incude '_' as well ...
+      if(servicename.charAt(0)==='_'){
+        secondunderscorepos = (servicename.substring(1)).indexOf('_');
+        if(secondunderscorepos>0){
+          secondunderscorepos++; //because of servicename's substring(1)
+          namespace = servicename.substring(1,secondunderscorepos);
+          servicename = servicename.substring(secondunderscorepos+1);
+          group = repo.alias+'_'+namespace+'_'+suffix+'s';
+        }
+      }
+      if(!group){
+        group = repo.alias+'_'+suffix+'s';
+      }
+
+
+      resultobj.result = {
+        modulename: mmn,
+        alias: repo.alias,
+        git : repo.git+group+'/'+servicename,
+        group: group,
+        servicename: servicename,
+        type: suffix,
+        namespace: namespace
+      };
+      return true;
+    }
+  }
+  function recognizeAllex(missingmodulename, additional_suffices, default_suffix){
+    if(!missingmodulename){
+      return null;
+    }
+
+    if (missingmodulename.indexOf(':') > -1) {
+      //TODO: ovaj nece proveravati suffix ... i da suffix bude blah, njemu ce to biti ok ... fer? mislim da ne ...
+      return shortNotationExpander(missingmodulename, default_suffix);
+    } 
+
+    var _index = missingmodulename.indexOf('_');
+    if (_index < 1) return null;
+    var repo = missingmodulename.substring (0, _index),
+      aliasdata = getAlias(repo),
+      suffices;
+
+    if (!aliasdata.git) return null;
+
+    resultobj = {result:null};
+
+    if (additional_suffices) {
+      suffices = _allexsuffices.concat(additional_suffices);
+    }else{
+      suffices = _allexsuffices;
+    }
+
+    suffices.some(checkForSuffix.bind(null, aliasdata, missingmodulename,resultobj));
+    return resultobj.result;
+  }
+
+  return recognizeAllex;
+
+}
+
+module.exports = createModuleRecognition;
---
+++ test/test.js
@@ -0,0 +1,44 @@
+var expect = require('chai').expect,
+  lib = require('..')(fakeGetAlias);
+
+function fakeGetAlias(alias) {
+  return {
+    alias: alias,
+    git: 'git@gitlab.hers.rs:'
+  };
+}
+
+describe('Test', function () {
+  it ('service', function () {
+    var r = lib('allex:master');
+    expect(r.servicename).to.equal('master');
+    expect(r.modulename).to.equal('allex_masterservice');
+    expect(r.type).to.equal('service');
+    expect(r.group).to.equal('allex_services');
+    expect(r.namespace).to.be.undefined;
+  });
+  it ('lib', function () {
+    var r = lib('allex:leveldb:lib');
+    expect(r.servicename).to.equal('leveldb');
+    expect(r.modulename).to.equal('allex_leveldblib');
+    expect(r.type).to.equal('lib');
+    expect(r.group).to.equal('allex_libs');
+    expect(r.namespace).to.be.undefined;
+  });
+  it ('namespace service', function () {
+    var r = lib('allex:lottery_slot');
+    expect(r.servicename).to.equal('slot');
+    expect(r.modulename).to.equal('allex__lottery_slotservice');
+    expect(r.type).to.equal('service');
+    expect(r.group).to.equal('allex_lottery_services');
+    expect(r.namespace).to.equal('lottery');
+  });
+  it ('namespace lib', function () {
+    var r = lib('allex:indata_fix:lib');
+    expect(r.servicename).to.equal('fix');
+    expect(r.modulename).to.equal('allex__indata_fixlib');
+    expect(r.type).to.equal('lib');
+    expect(r.group).to.equal('allex_indata_libs');
+    expect(r.namespace).to.equal('indata');
+  });
+});
